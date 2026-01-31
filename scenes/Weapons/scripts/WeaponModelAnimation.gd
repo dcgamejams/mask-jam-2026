ca936742ddbe48487eedba64d6e6a3a6cccc630b
Justin Bailey <bursetpr@gmail.com>
2026-01-31 17:57:02 -0500

    particle effects when we shoot

--- scenes/Weapons/scripts/WeaponModelAnimation.gd
+++ scenes/Weapons/scripts/WeaponModelAnimation.gd
@@ -3,20 +3,27 @@ extends Node3D
 class_name WeaponModel
 
 @onready var animation_player = $AnimationPlayer
-@onready var animation_player_muzzle: AnimationPlayer = $Root/Skeleton3D/Barrel/Node3D/MuzzleFlash_01/AnimationPlayer
+
+@onready var particle_animation_player = $Root/Skeleton3D/Barrel/Barrels/MuzzleFlash_01/AnimationPlayer
+
 @onready var parent_node = get_parent()
 
 signal ShootingAnimationFinished
 
 func _ready() -> void:
 	Global.Shoot.connect(ShootSignal)
+	particle_animation_player.play("RESET")
+	
 
 func _playShootingAnimtion() -> void:
 	animation_player.play("ShootAndReload")
-	animation_player_muzzle.play('main')
+	particle_animation_player.play("main")
 
-func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	ShootingAnimationFinished.emit()
+	
 	
 func ShootSignal() -> void:
 	_playShootingAnimtion()
