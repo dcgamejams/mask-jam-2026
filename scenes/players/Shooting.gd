504af19ca1eaab013e3a807459c74decf0646f33
Andrew Davis <jonandrewdavis@gmail.com>
2026-02-01 11:46:30 -0500

    tweak capture

--- scenes/players/Shooting.gd
+++ scenes/players/Shooting.gd
@@ -15,8 +15,9 @@ var MaxAmmo:int = 10
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_LEFT:
-			#print("Left click at: ", event.position)
-			
+			if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED: 
+				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) #set mouse mode as captured
+
 			if canShoot:
 				if CurrentAmmo > 0:
 					#var ray_origin: Vector3 = camera.global_position
