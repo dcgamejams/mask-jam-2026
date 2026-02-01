b987fb366d782752ffbea98fcf1d27d2406f1de3
Andrew Davis <jonandrewdavis@gmail.com>
2026-02-01 13:50:12 -0500

    health drain color

--- scenes/players/player_ui.gd
+++ scenes/players/player_ui.gd
@@ -11,6 +11,7 @@ extends CanvasLayer
 @onready var camera_holder: CameraObject = %CameraHolder
 
 @onready var mask_timer: Timer = Timer.new()
+@onready var health_color_timer: Timer = Timer.new()
 
 func _ready():
 	health.signal_max_health_updated.connect(_on_health_max)
@@ -21,17 +22,27 @@ func _ready():
 	goat.health_system.signal_max_health_updated.emit(1000)
 	
 	add_child(mask_timer)
-	mask_timer.wait_time = 0.1
+	mask_timer.wait_time = 0.2
 	mask_timer.timeout.connect(on_mask_hurt)
+
+	add_child(health_color_timer)
+	health_color_timer.wait_time = 0.2
+	health_color_timer.one_shot = true
+	health_color_timer.timeout.connect(color_fix)
+
 
 	camera_holder.signal_mask_on.connect(mask_hurt_start)
 
+
+var ui_mask_on: bool = false
+
 func mask_hurt_start(is_currently_on):
-	print(is_currently_on)
+	ui_mask_on = is_currently_on
 	if is_currently_on:
 		mask_timer.start()
 	else:
 		mask_timer.stop()
+		color_fix()
 
 func on_mask_hurt():
 	health.damage(1, 88)
@@ -42,8 +53,13 @@ func _on_health_max(new_max):
 	health_bar.max_value = new_max
 	health_bar.value = new_max
 	health_label.text = str(new_max)
-	
+
+
 func _on_health_updated(next_health):
+	if next_health < health_bar.value:
+		health_bar["theme_override_styles/fill"].bg_color = Color.CORAL
+		health_color_timer.start()
+
 	health_bar.value = next_health
 	health_label.text = str(next_health)
 
@@ -54,8 +70,11 @@ func _on_goat_health_max(new_max):
 	goat_bar.value = new_max
 	goat_health_label.text = str(new_max)
 	
-func _on_goat_health_updated(next_health):
+func color_fix():
+	if not ui_mask_on:
+		health_bar["theme_override_styles/fill"].bg_color = Color.WHITE
 
+func _on_goat_health_updated(next_health):
 	goat_bar.value = next_health
 	goat_health_label.text = str(next_health)
 	if next_health <= 1000:
